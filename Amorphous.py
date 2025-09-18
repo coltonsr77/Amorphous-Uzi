1e9b6157700faa79cd7e34503307a2dfedbe2617
coltonsr77 <coltonr3@icloud.com>
2025-09-18 00:37:41 -0400

    Update Amorphous.py
    Fixed Little Mistake

--- Amorphous.py
+++ Amorphous.py
@@ -920,7 +920,7 @@ async def on_message(message):
 import socket
 
     HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
-    PORT = 1000     # Port to listen on (non-privileged ports are > 1023)
+    PORT = 10000     # Port to listen on (non-privileged ports are > 1023)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
